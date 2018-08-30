49409540e8d26a8b221231f6903c888596129838
Mason <mhancock5466@gmail.com>
2018-08-30 10:56:56 -0500

    Update

--- Hotel Stay/Hotel Stay/Source.cpp
+++ Hotel Stay/Hotel Stay/Source.cpp
@@ -19,7 +19,7 @@ int main()
 	cin >> daysStayed;
 	
 	totalBill = roomCharge * daysStayed + internetCharge * daysStayed;
-	cout << "Your Bill Will Be: $" << totalBill << "\nEnjoy your stay, " << name << "." << endl;
+	cout << "Your Bill Will Be: $" << totalBill << "\nEnjoy your stay, " << name << "!" << endl;
 
 	system("Pause");
 	return 0;
